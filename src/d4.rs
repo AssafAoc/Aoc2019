c63a38bbc95295547ffacca0d335a034964c91b6
Assaf Aoc <assafaoc@protonmail.com>
2020-11-24 10:04:17 +0200

    finished day 4

--- src/d4.rs
+++ src/d4.rs
@@ -29,27 +29,34 @@ impl Iterator for DigitIterator {
 }
 
 pub fn run() {
+    fn always_increase(digits: &[u8]) -> bool {
+        digits.windows(2).fold(true, |is_increasing, pair| is_increasing && pair[0] <= pair[1])
+    }
+    fn double_found(digits: &[u8]) -> bool {
+        digits.windows(2).fold(false, |has_double, pair| has_double || pair[0] == pair[1])
+    }
+    fn double_only(digits: &[u8]) -> bool {
+        digits.windows(4).fold(false, |double_only, quad| double_only || (quad[1] == quad[2] && quad[1] != quad[0] && quad[2] != quad[3]))
+            || (digits[0] == digits[1] && digits[1] != digits[2])
+            || (digits[digits.len() - 1] == digits[digits.len() - 2] && digits[digits.len() - 2] != digits[digits.len() - 3])
+    }
+
     let range = 136760u32..595730;
 
-    let mut good_passwords = 0;
+    let mut good_passwords_a = 0;
+    let mut good_passwords_b = 0;
 
     for i in range {
-        let mut it = DigitIterator::new(i).peekable();
+        let digits: Vec<u8> = DigitIterator::new(i).collect::<Vec<_>>();
 
-        let mut always_increase = true;
-        let mut double_found = false;
-        'inner: while let Some(d) = it.next() {
-            if let Some(next_d) = it.peek() {
-                always_increase = always_increase && d <= *next_d;
-                double_found = double_found || d == *next_d
-            } else {
-                break 'inner
+        if always_increase(&digits) && double_found(&digits) {
+            good_passwords_a += 1;
+            if double_only(&digits) {
+                good_passwords_b += 1;
             }
-        }
-        if always_increase && double_found {
-            good_passwords += 1;
         }
     }
 
-    println!("a: {}", good_passwords);
+    println!("a: {}", good_passwords_a);
+    println!("a: {}", good_passwords_b);
 }
